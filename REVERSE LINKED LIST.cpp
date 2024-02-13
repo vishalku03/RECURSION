54f7f0ffef2b577ad66c0105f9ab90411f1bd30c
Vishal Kumar <157531413+vishalku03@users.noreply.github.com>
2024-02-13 20:11:09 +0530

    Add files via upload

---
+++ REVERSE LINKED LIST.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+#include<bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+     ListNode *reverseList(ListNode *head)
+     {
+
+          if (head == NULL || head->next == NULL)
+               return head;
+          ListNode *prev = head;
+          ListNode *curr = prev->next;
+          head->next = NULL;
+
+          while (prev != NULL && curr != NULL)
+          {
+               ListNode *next = curr->next;
+               curr->next = prev;
+               prev = curr;
+               curr = next;
+          }
+          return prev;
+     }
+};
+
+//LEETCODE PROBLEM -  206
