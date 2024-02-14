1a71c50dddbae7696c8321158c9442e75f4c911c
Vishal Kumar <157531413+vishalku03@users.noreply.github.com>
2024-02-14 19:55:43 +0530

    Add files via upload

---
+++ POWER OF FOUR.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+     bool isPowerOfFour(int n)
+     {
+          if (n == 1)
+               return true;
+
+          if (n <= 0 || n % 4 != 0)
+               return false;
+
+          return isPowerOfFour(n / 4);
+     }
+};
+
+// LEETCODE PROBLEM - 342
