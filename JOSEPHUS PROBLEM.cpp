fdd7d7651a8edeb24f6debb625321e51c0192bbf
Vishal Kumar <157531413+vishalku03@users.noreply.github.com>
2024-02-14 21:36:35 +0530

    Add files via upload

---
+++ JOSEPHUS PROBLEM.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+     int josephus(int n, int k)
+     {
+          // Your code here
+          if (n == 1)
+               return 1;
+          return (josephus(n - 1, k) + k - 1) % n + 1;
+              
+     }
+};
+// GFG
