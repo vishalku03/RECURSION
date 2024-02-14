0f66e85d30721b13b7c7f33427176bace8745397
Vishal Kumar <157531413+vishalku03@users.noreply.github.com>
2024-02-14 20:49:10 +0530

    Add files via upload

---
+++ DECODING STRING .cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+     string solve(string s, int &index)
+     {
+          string ans;
+          while (index < s.size() && s[index] != ']')
+          {
+               if (!isdigit(s[index]))
+                    ans += s[index++];
+               else
+               {
+                    int n = 0;
+                    while (index < s.size() && isdigit(s[index]))
+                         n = n * 10 + s[index++] - '0';
+                    index++;
+                    string temp = solve(s, index);
+                    index++;
+                    while (n--)
+                         ans += temp;
+               }
+          }
+          return ans;
+     }
+     string decodeString(string s)
+     {
+          int index = 0;
+          return solve(s, index);
+     }
+};
+
+// LEETCODE PROBLEM - 394
